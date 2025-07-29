38e8bd6bc6eb9226c1cc31a839fe1dd962b07fad
Sindhu C <csindhu2802@gmail.com>
2025-07-29 20:58:40 +0530

    Update and rename Day - 6 -2 to Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,27 @@
+
+package arithmetic;
+
+public class ArithmeticOperation{
+	public int add(int a,int b )
+	{
+		return a + b;
+	}
+	
+	public int sub(int a,int b){
+		return a - b;
+	}
+	public int mul(int a,int b ){
+		return a * b;
+	}
+	public int div(int a,int b )
+	{
+		return a / b;
+	}
+	
+	public int mod(int a,int b){
+		return a % b;
+	}
+}
+
+
+
